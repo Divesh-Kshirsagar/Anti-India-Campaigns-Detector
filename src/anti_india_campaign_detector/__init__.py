14d1a5b2140abab40127efa9795739640a346960
Divesh Kshirsagar <diveshkshirsagar42@gmail.com>
2025-08-28 11:02:36 +0530

    Add social media automation strategy to documentation and initialize Twitter automation module

---
+++ src/anti_india_campaign_detector/__init__.py
@@ -0,0 +1,10 @@
+"""
+Anti-India Campaign Detector - Version 1.0
+Twitter Automation Module
+
+Main package initialization
+"""
+
+__version__ = "1.0.0"
+__author__ = "Security Team"
+__description__ = "Anti-India Campaign Detection System - Twitter Module"
